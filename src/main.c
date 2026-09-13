e44895fadcb6aa3c71dbe784c48b25594c3f7a57
SalmonPul <salmon.clock.pulse@gmail.com>
2026-09-13 20:45:20 +0900

    TODO: get value lsm6dsl

--- src/main.c
+++ src/main.c
@@ -1,4 +1,12 @@
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+#define IMU_PWR_PIN 8
+
+static struct sensor_value  gyro_x_out, gyro_y_out;
+
+static const struct device *const lsm6dsl_dev = DEVICE_DT_GET_ONE(st_lsm6dsl);
 
 int main(void)
 {
