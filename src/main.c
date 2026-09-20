58ac722970ef140dc5bbc52ca050afb457480ae6
SalmonPul <salmon.clock.pulse@gmail.com>
2026-09-20 15:03:42 +0900

    add lsm6dsl code

--- src/main.c
+++ src/main.c
@@ -2,14 +2,39 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 
-#define IMU_PWR_PIN 8
+#define STACK_SIZE 1024
+#define PRIORITY 7
 
-static struct sensor_value  gyro_x_out, gyro_y_out;
+static struct sensor_value gyro_x_out, gyro_y_out;
 
 static const struct device *const lsm6dsl_dev = DEVICE_DT_GET_ONE(st_lsm6dsl);
 
-int main(void)
+void get_lsm6dsl_value()
 {
-    printf("Hello World!");
-    return 0;
+    struct sensor_value odr_attr;
+
+    if (!device_is_ready(lsm6dsl_dev)) {
+        printf("sensor: device is not ready yet.");
+        return;
+    }
+
+    odr_attr.val1 = 104;
+    odr_attr.val2 = 0;
+
+    if (sensor_attr_set(
+        lsm6dsl_dev,
+        SENSOR_CHAN_GYRO_XYZ,
+        SENSOR_ATTR_SAMPLING_FREQUENCY,
+        &odr_attr) < 0) {
+            printf("Could not set sensor type and channel.");
+            return;
+        }
+    while(1) {
+        sensor_sample_fetch(lsm6dsl_dev);
+        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_X, &gyro_x_out);
+        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_Y, &gyro_y_out);
+        printk("gyro_x: {d}.{d} gyro_y: {d}.{d}", gyro_x_out.val1, gyro_x_out.val2, gyro_y_out.val1, gyro_y_out.val2);
+    }
 }
+
+K_THREAD_DEFINE(get_lsm6dsl_value_id, STACK_SIZE, get_lsm6dsl_value, NULL, NULL, NULL, PRIORITY, 0, 0);
