22732f81df85c905aef5966e60ec3355eff52cfb
SalmonPul <salmon.clock.pulse@gmail.com>
2026-09-21 11:22:39 +0900

    Switch shared gyro variable to k_msgq for thread communication

--- src/main.c
+++ src/main.c
@@ -5,16 +5,23 @@
 #define STACK_SIZE 1024
 #define PRIORITY 7
 
-static struct sensor_value gyro_x_out, gyro_y_out;
+struct sensor_data {
+    struct sensor_value x;
+    struct sensor_value y;
+    struct sensor_value z;
+};
+
+K_MSGQ_DEFINE_STATIC(gyro_value, sizeof(struct sensor_data), 10, 4);
 
 static const struct device *const lsm6dsl_dev = DEVICE_DT_GET_ONE(st_lsm6dsl);
 
 void get_lsm6dsl_value()
 {
     struct sensor_value odr_attr;
+    struct sensor_data gyro_set;
 
     if (!device_is_ready(lsm6dsl_dev)) {
-        printf("sensor: device is not ready yet.");
+        printk("sensor: device is not ready yet.\n");
         return;
     }
 
@@ -26,15 +33,21 @@ void get_lsm6dsl_value()
         SENSOR_CHAN_GYRO_XYZ,
         SENSOR_ATTR_SAMPLING_FREQUENCY,
         &odr_attr) < 0) {
-            printf("Could not set sensor type and channel.");
+            printk("Could not set sensor type and channel.\n");
             return;
         }
     while(1) {
         sensor_sample_fetch(lsm6dsl_dev);
-        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_X, &gyro_x_out);
-        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_Y, &gyro_y_out);
-        printk("gyro_x: {d}.{d} gyro_y: {d}.{d}", gyro_x_out.val1, gyro_x_out.val2, gyro_y_out.val1, gyro_y_out.val2);
+        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_X, &gyro_set.x);
+        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_Y, &gyro_set.y);
+        sensor_channel_get(lsm6dsl_dev, SENSOR_CHAN_GYRO_Z, &gyro_set.z);
+        printk("gyro_x: %d.%d gyro_y: %d.%d gyro_z:%d.%d\n",
+            gyro_set.x.val1, gyro_set.x.val2,
+            gyro_set.y.val1, gyro_set.y.val2,
+            gyro_set.z.val1, gyro_set.z.val2);
     }
+
+    k_msgq_put(&gyro_value, &gyro_set, K_NO_WAIT);
 }
 
 K_THREAD_DEFINE(get_lsm6dsl_value_id, STACK_SIZE, get_lsm6dsl_value, NULL, NULL, NULL, PRIORITY, 0, 0);
